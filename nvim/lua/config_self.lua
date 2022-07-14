77e1bcea5d0b26958d8dd07a0ce9fe7aec064fd8
dolphinxyz <robimalco@gmail.com>
2022-07-14 22:32:39 +0200

    improve function

--- nvim/lua/config_self.lua
+++ nvim/lua/config_self.lua
@@ -1,10 +1,20 @@
 local vim = vim
 
-function SistemaAccentiItaliani()
-  vim.cmd(":%s/E'/É/g")
-  vim.cmd(":%s/a'/á/g")
-  vim.cmd(":%s/e'/é/g")
-  vim.cmd(":%s/i'/í/g")
-  vim.cmd(":%s/o'/ó/g")
-  vim.cmd(":%s/u'/ú/g")
+function SistemaAccenti()
+  local from = {"E'", "a'", "e'", "i'", "o'", "u'"}
+  local to = {"é", "á", "é", "í", "ó", "ú"}
+
+  local last_line = vim.api.nvim_buf_line_count(0)
+  local lines = vim.api.nvim_buf_get_lines(0, 0, last_line, true)
+
+  local s_lines = ""
+  for _, k in ipairs(lines) do
+    s_lines = s_lines .. k
+  end
+
+  for i in ipairs(from) do
+    if s_lines:match(from[i], 0) then
+      vim.cmd(":%s/"..from[i].."/"..to[i].."/g")
+    end
+  end
 end
