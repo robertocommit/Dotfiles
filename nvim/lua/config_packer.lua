11a86cd511909a74def9c146de5e42cb1d8ad55b
robertocommit <r@milhos.tech>
2024-11-09 13:25:00 +0100

    avante

--- nvim/lua/config_packer.lua
+++ nvim/lua/config_packer.lua
@@ -37,7 +37,7 @@ require('packer').startup(function()
   use 'fatih/vim-go' -- golang
 
   use { -- telescope
-    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
 
@@ -82,5 +82,48 @@ require('packer').startup(function()
       require("telescope").load_extension("dbtpal")
     end
   })
+
+
+
+use {
+  'yetone/avante.nvim',
+  requires = {
+    'nvim-treesitter/nvim-treesitter',
+    'stevearc/dressing.nvim',
+    'nvim-lua/plenary.nvim',
+    'MunifTanjim/nui.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'zbirenbaum/copilot.lua',
+    {
+      'HakonHarnes/img-clip.nvim',
+      config = function()
+        require('img-clip').setup({
+          -- your config
+        })
+      end
+    },
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      config = function()
+        require('render-markdown').setup({
+          file_types = { "markdown", "Avante" }
+        })
+      end
+    }
+  },
+  config = function()
+    require('copilot').setup({
+      -- your config
+    })
+    require('avante_lib').load()
+    require('avante').setup({
+      -- your config
+    })
+  end
+}
+
+
+
+
 
 end)
--- nvim/plugin/packer_compiled.lua
+++ nvim/plugin/packer_compiled.lua
@@ -49,8 +49,8 @@ local function save_profiles(threshold)
 end
 
 time([[Luarocks path setup]], true)
-local package_path_str = "/Users/roberto/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?.lua;/Users/roberto/.cache/nvim/packer_hererocks/2.1.1703358377/share/lua/5.1/?/init.lua;/Users/roberto/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?.lua;/Users/roberto/.cache/nvim/packer_hererocks/2.1.1703358377/lib/luarocks/rocks-5.1/?/init.lua"
-local install_cpath_pattern = "/Users/roberto/.cache/nvim/packer_hererocks/2.1.1703358377/lib/lua/5.1/?.so"
+local package_path_str = "/Users/roberto/.cache/nvim/packer_hererocks/2.1.1727870382/share/lua/5.1/?.lua;/Users/roberto/.cache/nvim/packer_hererocks/2.1.1727870382/share/lua/5.1/?/init.lua;/Users/roberto/.cache/nvim/packer_hererocks/2.1.1727870382/lib/luarocks/rocks-5.1/?.lua;/Users/roberto/.cache/nvim/packer_hererocks/2.1.1727870382/lib/luarocks/rocks-5.1/?/init.lua"
+local install_cpath_pattern = "/Users/roberto/.cache/nvim/packer_hererocks/2.1.1727870382/lib/lua/5.1/?.so"
 if not string.find(package.path, package_path_str, 1, true) then
   package.path = package.path .. ';' .. package_path_str
 end
@@ -80,6 +80,12 @@ _G.packer_plugins = {
     path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/Comment.nvim",
     url = "https://github.com/numToStr/Comment.nvim"
   },
+  ["avante.nvim"] = {
+    config = { "\27LJ\2\n|\0\0\3\0\6\0\0186\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\0016\0\0\0'\2\3\0B\0\2\0029\0\4\0B\0\1\0016\0\0\0'\2\5\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\vavante\tload\15avante_lib\nsetup\fcopilot\frequire\0" },
+    loaded = true,
+    path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/avante.nvim",
+    url = "https://github.com/yetone/avante.nvim"
+  },
   ["barbar.nvim"] = {
     loaded = true,
     path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/barbar.nvim",
@@ -100,11 +106,21 @@ _G.packer_plugins = {
     path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/context.vim",
     url = "https://github.com/wellle/context.vim"
   },
+  ["copilot.lua"] = {
+    loaded = true,
+    path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/copilot.lua",
+    url = "https://github.com/zbirenbaum/copilot.lua"
+  },
   dbtpal = {
-    config = { "\27LJ\2\n¤\2\0\0\3\0\6\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\0016\0\0\0'\2\4\0B\0\2\0029\0\5\0'\2\1\0B\0\2\1K\0\1\0\19load_extension\14telescope\1\0\5\24path_to_dbt_project+/Users/roberto/Code/Partners/Etiql/dbt\16path_to_dbt\26/opt/homebrew/bin/dbt\27protect_compiled_files\2\25extended_path_search\2\29path_to_dbt_profiles_dir\24/Users/roberto/.dbt\nsetup\vdbtpal\frequire\0" },
+    config = { "\27LJ\2\n¤\2\0\0\3\0\6\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\0016\0\0\0'\2\4\0B\0\2\0029\0\5\0'\2\1\0B\0\2\1K\0\1\0\19load_extension\14telescope\1\0\5\29path_to_dbt_profiles_dir\24/Users/roberto/.dbt\24path_to_dbt_project+/Users/roberto/Code/Partners/Etiql/dbt\16path_to_dbt\26/opt/homebrew/bin/dbt\27protect_compiled_files\2\25extended_path_search\2\nsetup\vdbtpal\frequire\0" },
     loaded = true,
     path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/dbtpal",
     url = "https://github.com/PedramNavid/dbtpal"
+  },
+  ["dressing.nvim"] = {
+    loaded = true,
+    path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/dressing.nvim",
+    url = "https://github.com/stevearc/dressing.nvim"
   },
   fzf = {
     loaded = true,
@@ -115,6 +131,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/fzf.vim",
     url = "https://github.com/junegunn/fzf.vim"
+  },
+  ["img-clip.nvim"] = {
+    config = { "\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rimg-clip\frequire\0" },
+    loaded = true,
+    path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/img-clip.nvim",
+    url = "https://github.com/HakonHarnes/img-clip.nvim"
   },
   indentLine = {
     loaded = true,
@@ -130,6 +152,11 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/mason.nvim",
     url = "https://github.com/williamboman/mason.nvim"
+  },
+  ["nui.nvim"] = {
+    loaded = true,
+    path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/nui.nvim",
+    url = "https://github.com/MunifTanjim/nui.nvim"
   },
   ["nvim-lspconfig"] = {
     loaded = true,
@@ -171,6 +198,12 @@ _G.packer_plugins = {
     loaded = true,
     path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/promise-async",
     url = "https://github.com/kevinhwang91/promise-async"
+  },
+  ["render-markdown.nvim"] = {
+    config = { "\27LJ\2\nw\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\15file_types\1\0\1\15file_types\0\1\3\0\0\rmarkdown\vAvante\nsetup\20render-markdown\frequire\0" },
+    loaded = true,
+    path = "/Users/roberto/.local/share/nvim/site/pack/packer/start/render-markdown.nvim",
+    url = "https://github.com/MeanderingProgrammer/render-markdown.nvim"
   },
   ["solarized-osaka.nvim"] = {
     loaded = true,
@@ -225,18 +258,30 @@ _G.packer_plugins = {
 }
 
 time([[Defining packer_plugins]], false)
--- Config for: dbtpal
-time([[Config for dbtpal]], true)
-try_loadstring("\27LJ\2\n¤\2\0\0\3\0\6\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\0016\0\0\0'\2\4\0B\0\2\0029\0\5\0'\2\1\0B\0\2\1K\0\1\0\19load_extension\14telescope\1\0\5\24path_to_dbt_project+/Users/roberto/Code/Partners/Etiql/dbt\16path_to_dbt\26/opt/homebrew/bin/dbt\27protect_compiled_files\2\25extended_path_search\2\29path_to_dbt_profiles_dir\24/Users/roberto/.dbt\nsetup\vdbtpal\frequire\0", "config", "dbtpal")
-time([[Config for dbtpal]], false)
--- Config for: oil.nvim
-time([[Config for oil.nvim]], true)
-try_loadstring("\27LJ\2\n1\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\boil\frequire\0", "config", "oil.nvim")
-time([[Config for oil.nvim]], false)
+-- Config for: img-clip.nvim
+time([[Config for img-clip.nvim]], true)
+try_loadstring("\27LJ\2\n:\0\0\3\0\3\0\a6\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\nsetup\rimg-clip\frequire\0", "config", "img-clip.nvim")
+time([[Config for img-clip.nvim]], false)
 -- Config for: Comment.nvim
 time([[Config for Comment.nvim]], true)
 try_loadstring("\27LJ\2\n5\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\fComment\frequire\0", "config", "Comment.nvim")
 time([[Config for Comment.nvim]], false)
+-- Config for: dbtpal
+time([[Config for dbtpal]], true)
+try_loadstring("\27LJ\2\n¤\2\0\0\3\0\6\0\r6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\3\0B\0\2\0016\0\0\0'\2\4\0B\0\2\0029\0\5\0'\2\1\0B\0\2\1K\0\1\0\19load_extension\14telescope\1\0\5\29path_to_dbt_profiles_dir\24/Users/roberto/.dbt\24path_to_dbt_project+/Users/roberto/Code/Partners/Etiql/dbt\16path_to_dbt\26/opt/homebrew/bin/dbt\27protect_compiled_files\2\25extended_path_search\2\nsetup\vdbtpal\frequire\0", "config", "dbtpal")
+time([[Config for dbtpal]], false)
+-- Config for: render-markdown.nvim
+time([[Config for render-markdown.nvim]], true)
+try_loadstring("\27LJ\2\nw\0\0\4\0\6\0\t6\0\0\0'\2\1\0B\0\2\0029\0\2\0005\2\4\0005\3\3\0=\3\5\2B\0\2\1K\0\1\0\15file_types\1\0\1\15file_types\0\1\3\0\0\rmarkdown\vAvante\nsetup\20render-markdown\frequire\0", "config", "render-markdown.nvim")
+time([[Config for render-markdown.nvim]], false)
+-- Config for: avante.nvim
+time([[Config for avante.nvim]], true)
+try_loadstring("\27LJ\2\n|\0\0\3\0\6\0\0186\0\0\0'\2\1\0B\0\2\0029\0\2\0004\2\0\0B\0\2\0016\0\0\0'\2\3\0B\0\2\0029\0\4\0B\0\1\0016\0\0\0'\2\5\0B\0\2\0029\0\2\0004\2\0\0B\0\2\1K\0\1\0\vavante\tload\15avante_lib\nsetup\fcopilot\frequire\0", "config", "avante.nvim")
+time([[Config for avante.nvim]], false)
+-- Config for: oil.nvim
+time([[Config for oil.nvim]], true)
+try_loadstring("\27LJ\2\n1\0\0\3\0\3\0\0066\0\0\0'\2\1\0B\0\2\0029\0\2\0B\0\1\1K\0\1\0\nsetup\boil\frequire\0", "config", "oil.nvim")
+time([[Config for oil.nvim]], false)
 
 _G._packer.inside_compile = false
 if _G._packer.needs_bufread == true then
